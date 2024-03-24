0ef496d311ce0a16cfc14bad1414beefc6077674
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-03-24 22:58:02 +0300

    Add files via upload

---
+++ Tek.java
@@ -0,0 +1,13 @@
+package tekbagli;
+
+public class Tek {
+
+	public static void main(String[] args) {
+		Liste listem = new Liste();
+		listem.basaEkle(37);
+		listem.sonaEkle(98);
+		listem.araEkle(45);
+		listem.yazdir();
+	}
+
+}
